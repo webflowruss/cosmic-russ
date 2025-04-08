e19105b40d0cf9536c5660d7c47741d6c3d7274b
Nate Selvidge <nate.selvidge@webflow.com>
2025-04-08 14:52:19 -0700

    update basePath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/app",
+  basePath: "/my-app",
 };
 
 export default nextConfig;
