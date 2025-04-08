3a6d85d4fc65d5a5669a48440b8bfa2ed5dd46d0
Nate Selvidge <nate.selvidge@webflow.com>
2025-04-08 12:35:24 -0700

    hard code base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: process.env.MOUNT_PATH || "",
+  basePath: "/app",
 };
 
 export default nextConfig;
