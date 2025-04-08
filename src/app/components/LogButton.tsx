706ced1fec644e53df1a5956309bc160fa400b0c
Joao Pedro <joao.silva@webflow.com>
2025-04-08 14:44:25 +0100

    test

--- src/app/components/LogButton.tsx
+++ src/app/components/LogButton.tsx
@@ -8,7 +8,7 @@ export default function LogButton() {
 
   const fetchMessage = async () => {
     try {
-      const response = await fetch('/app/api/hello');
+      const response = await fetch('/api/hello');
       const data = await response.json();
       console.log(data.message);
       setMessage(data.message);
