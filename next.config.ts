8ad1456f3d87076a6bf89b2886267c1451547f16
Nate Selvidge <nate.selvidge@webflow.com>
2025-04-09 10:20:53 -0700

    change basePath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/my-app",
+  basePath: "/app",
 };
 
 export default nextConfig;
