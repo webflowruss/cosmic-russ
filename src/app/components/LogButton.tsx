ec3f7c714808532499b1fc0804c8f855cb321b46
Joao Pedro <joao.silva@webflow.com>
2025-04-16 18:55:27 +0100

    update api call

--- src/app/components/LogButton.tsx
+++ src/app/components/LogButton.tsx
@@ -2,13 +2,14 @@
 
 import { useState } from 'react';
 import styles from '../page.module.css';
+import config from '../../../next.config';
 
 export default function LogButton() {
   const [message, setMessage] = useState<string>('');
 
   const fetchMessage = async () => {
     try {
-      const response = await fetch('/app/api/hello');
+      const response = await fetch(`${config.basePath}/api/hello`);
       const data = await response.json();
       setMessage(data.message);
     } catch (error) {
