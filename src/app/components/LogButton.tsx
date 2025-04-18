d89836cfc6b10cbff106f35c7af0f583bca768f9
Max Choate <176334032+maxcwebflow@users.noreply.github.com>
2025-04-18 16:35:49 -0700

    Bump opennext

--- src/app/components/LogButton.tsx
+++ src/app/components/LogButton.tsx
@@ -1,31 +1,28 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import styles from '../page.module.css';
-import config from '../../../next.config';
+import { useState } from "react";
+import styles from "../page.module.css";
+import config from "../../../next.config";
 
 export default function LogButton() {
-  const [message, setMessage] = useState<string>('');
+  const [message, setMessage] = useState<string>("");
 
   const fetchMessage = async () => {
     try {
       const response = await fetch(`${config.basePath}/api/hello`);
-      const data = await response.json();
+      const data = (await response.json()) as any;
       setMessage(data.message);
     } catch (error) {
-      console.error('Error fetching message:', error);
+      console.error("Error fetching message:", error);
     }
   };
 
   return (
     <div>
-      <button 
-        onClick={fetchMessage}
-        className={styles.secondary}
-      >
+      <button onClick={fetchMessage} className={styles.secondary}>
         Log Message
       </button>
       {message && <p className={styles.message}>{message}</p>}
     </div>
   );
-} 
+}
