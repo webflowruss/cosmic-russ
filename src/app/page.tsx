2c6f0f4680cfdfc4e87f4f38197af10defdaa5c3
Russ Nealis <russell.nealis@webflow.com>
2025-04-24 15:45:36 -0700

    emerson is awesome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Cosmic is Awesome!!</h1>
+        <h1 className={styles.title}>Emerson is Awesome!!</h1>
         <LogButton />
       </main>
     </div>
